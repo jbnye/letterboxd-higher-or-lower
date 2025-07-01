54a39dfc515694e5d646973b1b99c1e20d38cfcc
Jacob Nye <jn3268@gmail.com>
2025-06-30 22:18:24 -0400

    working on functiroanltiy of the game flow, game si technically playable at the moment, tested it and seemed ok

--- frontend/src/Components/LostPage.tsx
+++ frontend/src/Components/LostPage.tsx
@@ -1,4 +1,28 @@
+import {useState} from "react";
+import DifficultyBoxes from "./DifficultyBoxes";
+import type { Difficulty } from "../types/types";
 
-export default function LostPage(){
-    return (<></>);
+interface LostageProps {
+    onStartGame: (difficulty: Difficulty) => void,
+    finalScore: number
+}
+
+export default function LostPage({onStartGame, finalScore}: LostageProps){
+    const [difficultyPicked, setDifficultyPicked] = useState<Difficulty>("Easy");
+    return(
+        <div>
+            <h1 className="justify-self-center">{`Final Score: ${finalScore}`}</h1>
+            <DifficultyBoxes 
+            onDifficultyChoice={(difficulty) => {
+                setDifficultyPicked(difficulty);
+            }} 
+            difficultyPicked={difficultyPicked}
+            />
+
+            <button onClick={() => onStartGame(difficultyPicked)}
+            className="mt-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+                Play Again
+            </button>
+        </div>
+    )
 }
