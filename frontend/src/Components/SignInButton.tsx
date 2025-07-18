d2dd410aed0f0a171304e26c6a313815f6ce9238
Jacob Nye <jn3268@gmail.com>
2025-07-18 13:48:41 -0400

    hover on difficulty boxes changes opacity

---
+++ frontend/src/Components/SignInButton.tsx
@@ -0,0 +1,13 @@
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+
+export default function GoogleSignInButton() {
+
+
+    return (
+        <>
+        <button>
+
+        </button>
+        </>
+    )
+}
