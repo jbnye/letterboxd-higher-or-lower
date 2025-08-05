51c929207f55595bac6209e2d8931e8586f81f18
Jacob Nye <jn3268@gmail.com>
2025-08-05 19:51:27 -0400

    css and audio functions

--- frontend/src/Util/utilityFunctions.ts
+++ frontend/src/Util/utilityFunctions.ts
@@ -1,5 +1,17 @@
 
 
 export function capitalizeFirst(str: string) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function playTimeoutSound() {
+    const audio = new Audio("/Audio/lichess-beep.mp3");
+    audio.volume = 0.3;
+    audio.play();
+}
+
+export function playDefeatSound() {
+    const audio = new Audio("/Audio/Wrong.mp3");
+    audio.volume = 0.3;
+    audio.play();
 }
