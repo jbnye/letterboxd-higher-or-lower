2d508d4350dca157c28d338fb204032404d19294
Jacob Nye <jn3268@gmail.com>
2025-08-11 23:24:41 -0400

    added mobile/tablet/desktop in ThemeContext

--- frontend/src/Context/ThemeStatus.tsx
+++ frontend/src/Context/ThemeStatus.tsx
@@ -1,9 +1,9 @@
 import {createContext, useContext, useState, useEffect, useMemo} from "react";
-
-
+import type { windowBreakpoints } from "@/types/types";
 interface ThemeStatusProps{
     darkMode: boolean;
     setDarkMode: (select: boolean) => void;
+    breakpoint: windowBreakpoints;
 }
 
 
@@ -17,18 +17,27 @@ export const ThemeStatusProvider = ({children}: ({children: React.ReactNode})) =
         if(stored === 'false'){return false;}
         return window.matchMedia('(prefers-color-scheme: dark)').matches;
     });
-    // console.log(darkMode);
+    const [breakpoint, setBreakpoint] = useState<"mobile" | "tablet" | "desktop">("desktop");
+    console.log(breakpoint);
+    useEffect(()=> {
+        const checkBreakpoint = () => {
+            const width = window.innerWidth;
+            if(width <= 640) setBreakpoint("mobile");
+            else if(width<=1024) setBreakpoint("tablet");
+            else setBreakpoint("desktop");
+        }
+        checkBreakpoint();
+        window.addEventListener("resize", checkBreakpoint);
+        return () => window.removeEventListener("resize", checkBreakpoint);
+    },[]);
 
     useEffect(() => {
-        localStorage.setItem('darkMode', darkMode.toString());
-        if(darkMode){
-            document.documentElement.classList.add('dark');
-        }else{
-            document.documentElement.classList.remove('dark');
-        }
-    },[darkMode]);
+        localStorage.setItem("darkMode", darkMode.toString());
+        document.documentElement.classList.toggle("dark", darkMode);
+    }, [darkMode]);
 
-    const value = useMemo(() => ({darkMode, setDarkMode}), [darkMode]);
+
+    const value = useMemo(() => ({darkMode, setDarkMode, breakpoint}), [darkMode, breakpoint]);
     return(
         <ThemeStatus.Provider value={value}>
             {children}
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -4,6 +4,7 @@ export type AuthStatus = "checking" | "authenticated" | "not-authenticated";
 export type ServerStatus = 'checking' | 'online' | 'offline';
 export type RatingStatus = "secret" | "animating" | "revealed";
 export type ColorState = "none" | "correct" | "incorrect";
+export type windowBreakpoints = "mobile" | "tablet" | "desktop";
 export type Highscores = {
   easy: number;
   medium: number;
